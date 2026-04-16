7387923b36e713334a8d0b8f1d7f09e6dea8a570
rahulmuniganti-ux <rahulmuniganti75@gmail.com>
2026-04-16 14:50:39 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,24 +2,26 @@ import fitz
 import pandas as pd
 import re
 
-def extract_timetable(file_path):
 
-    doc = fitz.open(file_path)
+def extract_timetable(file_path):
+    try:
+        doc = fitz.open(file_path)
+    except Exception as e:
+        print("Error opening file:", e)
+        return pd.DataFrame()
+
     text = ""
 
     for page in doc:
         text += page.get_text()
 
     lines = text.split("\n")
-
     data = []
 
     for line in lines:
-
         date = re.search(r"\d{2}-\d{2}-\d{4}", line)
 
         if date:
-
             subject = line.replace(date.group(), "").strip()
 
             if len(subject) < 3:

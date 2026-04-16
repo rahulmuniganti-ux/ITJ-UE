32c4177946b53ef517e695f0840142ab5b7f2592
rahulmuniganti-ux <rahulmuniganti75@gmail.com>
2026-04-16 15:06:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -233,5 +233,8 @@ def delete_file(filename):
     return redirect("/history")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
